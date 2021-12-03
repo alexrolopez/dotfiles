d65ed03e9c0e772cf7ad8dece1a5c91fb2e20175
Alex Rodriguez Lopez <aliksandr@gmail.com>
2021-12-03 01:47:27 +0100

    1st working version (just git and apt for now)

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Alex's dotfiles
+Development environment for any apt-based linux distro (Ubuntu, Debian, etc).
+Using ansible for the whole installation, plus a small bootstrap script (install.sh).
 
-```sh
-$ cd
-$ git clone git@github.com:alexrolopez/dotfiles .dotfiles
-$ .dotfiles/bin/dot-bootstrap
+## Included roles
+- Git: Installs git if needed, along a minimal global gitconfig and gitignore.
+- Apt: Installs base packages.
+
+```bash
+$ git clone git@github.com:alexrolopez/dotfiles ~/.dotfiles
+
+# 1st time, 0 requirements setup:
+$ GIT_USER_NAME="My Name" GIT_USER_EMAIL="my_email@example.com" ~/.dotfiles/install.sh
+
+# Subsequent times, it is enough to run:
+$ ~/.dotfiles/install.sh
 ```
 
 ## Inspiration
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Sets up the dev environment, dotfiles, etc.
+# 1) Checks for the required env vars (git user name and email)
+# 2) Installs ansible if needed
+# 3) Executes dotfiles.yml playbook (all the roles, or optionally just tags passed as args)
+
+set -e
+
+function die {
+  (>&2 echo -e "\\033[1;91m[FATAL] $1\\033[0;0m")
+  exit 1
+}
+
+# Check required env vars
+
+if [ -z "${GIT_USER_NAME:=$(git config --global --get user.name)}" ]
+then
+  die "GIT_USER_NAME not set"
+fi
+
+if [ -z "${GIT_USER_EMAIL:=$(git config --global --get user.email)}" ]
+then
+  die "GIT_USER_EMAIL not set"
+fi
+
+export GIT_USER_NAME
+export GIT_USER_EMAIL
+
+tags="${1:-all}"
+
+if ! [ -x "$(command -v ansible)" ]; then
+  sudo apt update && sudo apt install -y ansible
+fi
+
+ansible-playbook -i ~/.dotfiles/hosts ~/.dotfiles/dotfiles.yml --ask-become-pass --tags "$tags"
+
+echo "Dotfiles installed successfully. Development environment ready!"
