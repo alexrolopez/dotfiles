4a36ba639376b7c0bab8fdf6f6c9d540b6127f07
Alex Rodriguez Lopez <aliksandr@gmail.com>
2021-12-27 22:40:43 +0100

    Alias v

--- roles/zsh/aliases.zsh
+++ roles/zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias q="cd ..; ls"
-alias vi="nvim"
 alias vim="nvim"
+alias vi="nvim"
+alias v="nvim"
 alias up="sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo apt autoclean"
