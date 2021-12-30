4f47853037d4373d28da1b5896f2234b2418ac9b
Alex Rodriguez Lopez <aliksandr@gmail.com>
2021-12-31 00:02:47 +0100

    Add zsh-autosuggestions, zsh-syntax-highlighting

--- roles/zsh/fzf.zsh
+++ roles/zsh/fzf.zsh
@@ -2,7 +2,7 @@ source /usr/share/doc/fzf/examples/key-bindings.zsh
 source /usr/share/doc/fzf/examples/completion.zsh
 
 export FZF_DEFAULT_COMMAND="fdfind --hidden --follow --exclude .git"
-export FZF_DEFAULT_OPTS="--bind='ctrl-u:preview-page-up' --bind='ctrl-d:preview-page-down'"
+export FZF_DEFAULT_OPTS="--bind='ctrl-b:preview-page-up' --bind='ctrl-f:preview-page-down'"
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_PREVIEW_COMMAND="batcat --style=numbers,changes --wrap=never --color=always {} || cat {} || tree -alC --noreport {}"
