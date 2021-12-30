05ee638979692e2043bfc25bba71e561e211f6fb
Alex Rodriguez Lopez <aliksandr@gmail.com>
2021-12-30 23:22:04 +0100

    Add proper fzf/bat/fd/tree integration

--- roles/zsh/fzf.zsh
+++ roles/zsh/fzf.zsh
@@ -1,6 +1,12 @@
 source /usr/share/doc/fzf/examples/key-bindings.zsh
 source /usr/share/doc/fzf/examples/completion.zsh
 
-export FZF_DEFAULT_COMMAND="fdfind --type f --hidden --follow --exclude .git"
+export FZF_DEFAULT_COMMAND="fdfind --hidden --follow --exclude .git"
+export FZF_DEFAULT_OPTS="--bind='ctrl-u:preview-page-up' --bind='ctrl-d:preview-page-down'"
+
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_CTRL_T_OPTS="--preview='[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (batcat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -30'"
+export FZF_CTRL_T_PREVIEW_COMMAND="batcat --style=numbers,changes --wrap=never --color=always {} || cat {} || tree -alC --noreport {}"
+export FZF_CTRL_T_OPTS="--preview '($FZF_CTRL_T_PREVIEW_COMMAND) 2> /dev/null'"
+
+export FZF_ALT_C_COMMAND='fdfind --type d --hidden --follow --exclude .git'
+export FZF_ALT_C_OPTS="--preview 'tree -alC --noreport {}'"
