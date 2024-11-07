3a76b2a54280009610a0a1061d9cbf06833c737b
Alex Rodriguez <alex.r@amenitiz.com>
2024-11-07 09:53:01 +0100

    Change install.sh to be compatible with debian and macos

--- install.sh
+++ install.sh
@@ -12,26 +12,43 @@ function die {
   exit 1
 }
 
-# Check required env vars
+function is_macos {
+  [[ "$(uname)" == "Darwin" ]]
+}
 
+function is_apt_based_linux {
+  [[ -f /etc/debian_version ]]
+}
+
+function is_ansible_installed {
+  [ -x "$(command -v ansible)" ]
+}
+
+# Check required env vars
 if [ -z "${GIT_USER_NAME:=$(git config --global --get user.name)}" ]
 then
   die "GIT_USER_NAME not set"
 fi
-
 if [ -z "${GIT_USER_EMAIL:=$(git config --global --get user.email)}" ]
 then
   die "GIT_USER_EMAIL not set"
 fi
-
 export GIT_USER_NAME
 export GIT_USER_EMAIL
 
-if ! [ -x "$(command -v ansible)" ]; then
-  sudo apt update && sudo apt install -y ansible
+# Install ansible if needed
+if is_macos; then
+  if ! is_ansible_installed; then
+    brew install ansible
+  fi
+elif is_apt_based_linux; then
+  if ! is_ansible_installed; then
+    sudo apt update && sudo apt install -y ansible
+  fi
+else
+  die "Unsupported platform"
 fi
 
-ansible-galaxy collection install community.postgresql
 ansible-playbook -i ~/.dotfiles/hosts ~/.dotfiles/dotfiles.yml --ask-become-pass
 
 echo "Development environment ready! Log out and log back in for new zsh shell to take effect."
